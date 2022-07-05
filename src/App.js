02008c9a11d9f95616c7937da0839feda1a6bff9
Antony <jns5@illinois.edu>
2022-07-05 15:45:34 -0500

    Implementing function to check if metamask is installed and wallet connected.  Starting to implement function to check if an ID is valid.

--- src/App.js
+++ src/App.js
@@ -1,23 +1,51 @@
-import logo from './logo.svg';
+import React, { useEffect, useState } from "react";
+import { ethers, utils } from "ethers";
 import './App.css';
 
+
 function App() {
+  const [currentAccount, setCurrentAccount] = useState("");
+  const [hasId, setHasId] = useState(false);
+  const [buttonText, setButtonText] = useState("");
+  const [description, setDescription] = useState("");
+
+  const contractAddress = ""; // to be added for the new contract that I make
+
+  const isMetaMaskInstalled = async () => {
+    const { ethereum } = window;
+    return Boolean(ethereum && ethereum.isMetaMask);
+  }
+
+  const checkIfWalletIsConnected = async () => {
+    if (isMetaMaskInstalled() === false) {
+      console.log("Get Metamask!");
+      alert("Get Metamask!");
+      return;
+      // only logging to console for now, can add functionality to display a link . . .
+    }
+    
+    const { ethereum } = window;
+
+    const accounts = await ethereum.request({ method: 'eth_accounts' });
+
+    if (accounts.length !== 0) {
+      const account = accounts[0];
+      console.log("Found an authorized account:", account);
+      setCurrentAccount(account);
+    } else {
+      console.log("No authorized account found");
+      // Will have to flash this on screen, not sure how this is possible
+    }
+  }
+
+  const verifyId = async () => {
+    
+  }
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
     </div>
   );
 }
